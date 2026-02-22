3cca0bb14bfa58f551d85b0f2a32181c1b4fc675
RihanC <crihan082@gmail.com>
2026-02-21 21:22:00 -0800

    added a text

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+      <h1>Welcome to live coding Challange with WCE.......</h1>
+    </div>
+  )
+}
+
+export default header
