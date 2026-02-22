a06c882756f70f79510491795e516bf351beddbe
Sanket-Thorat99 <sanketthorat9935@gmail.com>
2026-02-22 11:16:55 +0530

    Styling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div className="main">
-      <Header />
-        <Footer/>
-
-    </div>
+      <div className="main">
+        <Header />
+      </div>
+      <div className="body-content">
+        <Content />
+      </div>
+      <br></br>
+      <div className="footer">
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,19 @@
+const Content = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit libero
+        saepe illum molestias aliquam similique culpa qui illo iste, consectetur
+        praesentium, reprehenderit quasi nesciunt voluptatem repellat,
+        consequatur explicabo aliquid cumque. Expedita laudantium, nemo harum
+        sequi sit voluptatum quod nam. Vero, ipsam! Dolor reprehenderit neque
+        molestiae laborum illo voluptatum laboriosam excepturi placeat, autem
+        odit nulla beatae recusandae? Perspiciatis repellendus eligendi
+        pariatur. Molestiae, adipisci quod. Eius vero saepe earum officia
+        explicabo beatae modi ipsum nam, numquam quis, quod illum facilis
+      </p>
+    </>
+  );
+};
+
+export default Content;
