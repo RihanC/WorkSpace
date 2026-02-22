450d61f5882b138fc558a037ec850160f6257b55
RihanC <crihan082@gmail.com>
2026-02-21 22:07:14 -0800

    added index

---
+++ src/components/index.jsx
@@ -0,0 +1,17 @@
+//calculaor app interface mainly header and body
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h1>Calculator App</h1>
+
+        
+
+      
+    </div>
+  )
+}
+
+export default index
