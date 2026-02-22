2830acf9d127fe9cd02978680ff16e0de7aa6ad0
Swayam <officialswayamsahu01@gmail.com>
2026-02-22 11:33:40 +0530

    meow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 const Footer = () => {
-    return <><h4>This is our Footer component....</h4></>
+    return <><h4>This is our Footer component....</h4>
+    <p>Copyright@meow</p></>
 }
 
 export default Footer;
